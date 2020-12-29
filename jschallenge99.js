a5b6db92dbe06bf9788ba03c2d59059fcb683716
Malcolm Peterson <mgpetersn@gmail.com>
2020-12-29 11:06:08 -0500

    fcc Javascript Challenge (no.99)
    Basic JavaScript: Nesting For Loops

---
+++ jschallenge99.js
@@ -0,0 +1,15 @@
+//Basic JavaScript: Nesting For Loops ch.99
+
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+for(var i=0; i < arr.length; i++){
+  for(var j=0; j< arr.[i].length; j++){
+    product*=arr.[i][j];
+  }
+}
+}
+
+;
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
